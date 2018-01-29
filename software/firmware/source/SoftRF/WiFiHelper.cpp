8283e85da4ac8bc550e058b855873b0c9be33fca
Linar Yusupov <linar.r.yusupov@gmail.com>
2018-01-29 12:39:56 +0300

    Fix for hostname/SSID length.

--- software/firmware/source/SoftRF/WiFiHelper.cpp
+++ software/firmware/source/SoftRF/WiFiHelper.cpp
@@ -202,7 +202,7 @@ void WiFi_setup()
   }
 
   // Set Hostname.
-  host_name += String(SoC->getChipId(), HEX);
+  host_name += String((SoC->getChipId() & 0xFFFFFF), HEX);
   SoC->WiFi_hostname(host_name);
 
   // Print hostname.
