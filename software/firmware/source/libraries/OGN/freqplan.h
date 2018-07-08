03d8e212e9ec4bc44fde39daf3954fd3ccf9ff78
Linar Yusupov <linar.r.yusupov@gmail.com>
2018-07-08 20:13:45 +0300

    China's freq. plan is squeezed to comply with nRF905 limitations

--- software/firmware/source/libraries/OGN/freqplan.h
+++ software/firmware/source/libraries/OGN/freqplan.h
@@ -20,7 +20,7 @@ class FreqPlan
      else if(Plan==3) { BaseFreq=917000000; ChanSepar=400000; Channels=24; } // Australia and South America
      else if(Plan==4) { BaseFreq=869250000; ChanSepar=200000; Channels= 1; } // New Zealand
      else if(Plan==5) { BaseFreq=868800000; ChanSepar=200000; Channels= 1; } // Russia
-     else if(Plan==6) { BaseFreq=470200000; ChanSepar=400000; Channels= 1 /* 100 */; } // China, 470-510 MHz
+     else if(Plan==6) { BaseFreq=470100000; ChanSepar=200000; Channels= 1 /* 18 */; } // China, 470-473.6 MHz
 #if !defined(TEST_PAW_ON_NICERF_SV610_FW466)
      else if(Plan==7) { BaseFreq=869525000; ChanSepar=200000; Channels= 1; } // PilotAware (UK)
 #else
