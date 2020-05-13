799491062a14ab4db3dda03e7188a69b86e0b55f
Linar Yusupov <linar.r.yusupov@gmail.com>
2020-05-13 12:18:45 +0300

    insecure BLE OAD with CC1352 works good.

--- software/firmware/source/SoftRF/Platform_CC13XX.cpp
+++ software/firmware/source/SoftRF/Platform_CC13XX.cpp
@@ -176,8 +176,18 @@ static void CC13XX_loop()
 
 static void CC13XX_fini()
 {
+#if defined(ENERGIA_ARCH_CC13XX)
+    SysCtrlSystemReset();
+#elif defined(ENERGIA_ARCH_CC13X2)
+    /* Disable interrupts */
+    IntMasterDisable();
 
-  SysCtrlSystemReset();
+    SysCtrlStandby(false,
+                   VIMS_ON_CPU_ON_MODE,
+                   SYSCTRL_PREFERRED_RECHARGE_MODE);
+
+    /* Should never return from SysCtrlStandby */
+#endif /* ENERGIA_ARCH_CC13XX or ENERGIA_ARCH_CC13X2 */
 }
 
 static void CC13XX_reset()
@@ -476,5 +486,144 @@ const SoC_ops_t CC13XX_ops = {
   CC13XX_WDT_setup,
   CC13XX_WDT_fini
 };
+
+#if defined(ENERGIA_ARCH_CC13X2)
+
+/******************************************************************************
+
+ Copyright (c) 2014-2019, Texas Instruments Incorporated
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ *  Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+ *  Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+
+ *  Neither the name of Texas Instruments Incorporated nor the names of
+    its contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ *****************************************************************************/
+
+#include <ti/common/cc26xx/oad/oad_image_header.h>
+
+#define VECTOR_TB_SIZE       0x40 //!< Interrupt vector table entry size */
+#ifndef STACK_LIBRARY
+  #define BOUNDARY_SEG_LEN   0x18 //!< Length of the boundary segment */
+#endif
+
+#define SOFTWARE_VER            {'0', '0', '0', '1'}
+
+/*
+ * NV Page Setting:
+ * This define is used ensure the stack is built with a compatible NV setting
+ * Note: this restriction does not apply to the stack library configuration
+ * for off-chip OAD
+ */
+
+extern const uint32_t  RAM_END;
+
+#if defined HAL_IMAGE_A
+extern const uint8_t  ENTRY_END;
+extern const uint8_t  ENTRY_START;
+#endif
+
+/* This symbol is create by the linker file */
+extern uint8_t ramStartHere;
+extern uint8_t _intvecs_base_address;
+extern uint8_t ramStartHere;
+extern uint8_t __UNUSED_FLASH_start__;
+extern uint32_t heapEnd;
+extern uint32_t FLASH_END;
+
+__attribute__ ((section (".image_header"))) const imgHdr_t _imgHdr __attribute__((used)) =
+{
+  {
+    .imgID = OAD_IMG_ID_VAL,
+    .crc32 = DEFAULT_CRC,
+    .bimVer = BIM_VER,
+    .metaVer = META_VER,                   //!< Metadata version */
+    .techType = OAD_WIRELESS_TECH_BLE,     //!< Wireless protocol type BLE/TI-MAC/ZIGBEE etc. */
+    .imgCpStat = DEFAULT_STATE,            //!< Image copy status bytes */
+    .crcStat = DEFAULT_STATE,              //!< CRC status */
+#if (!defined(STACK_LIBRARY) && (defined(SPLIT_APP_STACK_IMAGE)))
+    .imgType = OAD_IMG_TYPE_APP,
+#else
+  #if defined HAL_IMAGE_A
+    .imgType =  OAD_IMG_TYPE_PERSISTENT_APP,
+  #else
+    .imgType = OAD_IMG_TYPE_APPSTACKLIB,
+  #endif
+#endif
+    .imgNo = 0x1,                          //!< Image number of 'image type' */
+    .imgVld = 0xFFFFFFFF,                  //!< In indicates if the current image in valid 0xff - valid, 0x00 invalid image */
+    .len = INVALID_LEN,                     //!< Image length in bytes. */
+    .prgEntry = (uint32_t)&_intvecs_base_address,
+    .softVer = SOFTWARE_VER,               //!< Software version of the image */
+    .imgEndAddr = (uint32_t)&__UNUSED_FLASH_start__,
+    .hdrLen = offsetof(imgHdr_t, fixedHdr.rfu) + sizeof(((imgHdr_t){0}).fixedHdr.rfu),   //!< Total length of the image header */
+    .rfu = 0xFFFF,                         //!< reserved bytes */
+  },
+
+#if (defined(SECURITY))
+  {
+    .segTypeSecure = IMG_SECURITY_SEG_ID,
+    .wirelessTech = OAD_WIRELESS_TECH_BLE,
+    .verifStat = DEFAULT_STATE,
+    .secSegLen = 0x55,
+    .secVer = SECURITY_VER,                     //!< Image payload and length */
+    .secTimestamp = 0x0,                         //!< Security timestamp */
+    .secSignerInfo = 0x0,
+  },
+#endif
+
+#if (!defined(STACK_LIBRARY) && (defined(SPLIT_APP_STACK_IMAGE)))
+  {
+    .segTypeBd = IMG_BOUNDARY_SEG_ID,
+    .wirelessTech1 = OAD_WIRELESS_TECH_BLE,
+    .rfu = DEFAULT_STATE,
+    .boundarySegLen = BOUNDARY_SEG_LEN,
+    .ram0StartAddr = (uint32_t)&ramStartHere,  //!< RAM entry start address */
+
+    #if defined HAL_IMAGE_A                    //! Persistent image */
+      .imgType =  OAD_IMG_TYPE_PERSISTENT_APP, //!< Persistent image Type */
+      .stackStartAddr = INVALID_ADDR,          //!< Stack start address */
+      .stackEntryAddr = INVALID_ADDR,
+    #else /* User application image */
+      .imgType =  OAD_IMG_TYPE_APP,            //!< Application image Type */
+      .stackEntryAddr = ICALL_STACK0_ADDR,
+      .stackStartAddr = ICALL_STACK0_START,
+    #endif /* defined HAL_IMAGE_A */
+      .imgType = OAD_IMG_TYPE_APP,
+  },
+#endif /* STACK_LIBRARY */
+
+  // Image payload segment initialization
+   {
+     .segTypeImg = IMG_PAYLOAD_SEG_ID,
+     .wirelessTech = OAD_WIRELESS_TECH_BLE,
+     .rfu = DEFAULT_STATE,
+     .startAddr = (uint32_t)&(_imgHdr.fixedHdr.imgID),
+   }
+ };
+#endif /* ENERGIA_ARCH_CC13X2 */
 
 #endif /* ENERGIA_ARCH_CC13XX || ENERGIA_ARCH_CC13X2 */
