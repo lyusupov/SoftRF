49011c5384d0f8a2662598edd72e2e20d59c22d9
Linar Yusupov <linar.r.yusupov@gmail.com>
2020-07-02 09:24:18 +0300

    Update of TTGO LoRa32 V2.X GNSS pins mapping [skip ci]

--- software/firmware/source/SoftRF/Platform_ESP32.h
+++ software/firmware/source/SoftRF/Platform_ESP32.h
@@ -178,9 +178,9 @@ extern Adafruit_NeoPixel strip;
 #define TTGO_V2_OLED_PIN_RST    U8X8_PIN_NONE // connected to CPU RST/EN
 #define TTGO_V2_OLED_PIN_SDA    21
 #define TTGO_V2_OLED_PIN_SCL    22
-#define TTGO_V2_PIN_GNSS_RX     32
-#define TTGO_V2_PIN_GNSS_TX     33
-#define TTGO_V2_PIN_GNSS_PPS    34
+#define TTGO_V2_PIN_GNSS_RX     34
+#define TTGO_V2_PIN_GNSS_TX     12
+#define TTGO_V2_PIN_GNSS_PPS    39
 
 // Hardware pin definitions for Heltec and TTGO-V1 LoRa-32 Boards with OLED SSD1306 I2C Display
 #define HELTEC_OLED_PIN_RST     U8X8_PIN_NONE // 16
