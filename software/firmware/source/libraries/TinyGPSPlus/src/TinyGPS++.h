500fadb4170cd9e80afc2012be5c13cc766ce903
Linar Yusupov <linar.r.yusupov@gmail.com>
2021-10-27 17:23:53 +0300

    misc. fix of TinyGPSPlus library [skip ci]

--- software/firmware/source/libraries/TinyGPSPlus/src/TinyGPS++.h
+++ software/firmware/source/libraries/TinyGPSPlus/src/TinyGPS++.h
@@ -42,7 +42,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define _GPS_MILES_PER_METER 0.00062137112
 #define _GPS_KM_PER_METER 0.001
 #define _GPS_FEET_PER_METER 3.2808399
-#define _GPS_MAX_FIELD_SIZE 15
+#define _GPS_MAX_FIELD_SIZE 33
 
 struct RawDegrees
 {
