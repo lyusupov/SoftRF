d30e0763a95e6e6fe0cf5d8dc24907378b4e2979
Linar Yusupov <linar.r.yusupov@gmail.com>
2022-05-15 14:46:10 +0300

    fix against regression of WebTop-ESP8266 build

--- software/firmware/source/libraries/TFT_eSPI/User_Setup_Select.h
+++ software/firmware/source/libraries/TFT_eSPI/User_Setup_Select.h
@@ -24,11 +24,11 @@
 #if defined(ESP32)
 #include "sdkconfig.h"
 #endif /* ESP32 */
-#if defined(CONFIG_IDF_TARGET_ESP32)
+#if !defined(CONFIG_IDF_TARGET_ESP32S2) && !defined(CONFIG_IDF_TARGET_ESP32S3)
 #include <User_Setups/Setup45_TTGO_T_Watch.h>      // Setup file for ESP32 and TTGO T-Watch ST7789 SPI bus TFT  240x240
-#elif defined(CONFIG_IDF_TARGET_ESP32S2) || defined(CONFIG_IDF_TARGET_ESP32S3)
+#else
 #include <User_Setups/Setup72_TTGO_T_Display_S2.h> // Setup file for ESP32-S2 and TTGO T-Display S2 ST7789 SPI TFT 135x240
-#endif /* CONFIG_IDF_TARGET_ESP32 */
+#endif /* CONFIG_IDF_TARGET_ESP32SX */
 
 //#include <User_Setups/Setup1_ILI9341.h>  // Setup file for ESP8266 configured for my ILI9341
 //#include <User_Setups/Setup2_ST7735.h>   // Setup file for ESP8266 configured for my ST7735
