6994a90edc39792317829ac323e606002b5b9138
Linar Yusupov <linar.r.yusupov@gmail.com>
2024-11-14 17:21:42 +0300

    Current state is good enough to qualify it as Release 1.6

--- software/firmware/source/SoftRF/SoftRF.h
+++ software/firmware/source/SoftRF/SoftRF.h
@@ -34,8 +34,8 @@
 #endif /* RASPBERRY_PI */
 
 #define SOFTRF_IDENT            "SoftRF"
-#define SOFTRF_FIRMWARE_VERSION "1.5.1"
-#define SOFTRF_USB_FW_VERSION   0x0105
+#define SOFTRF_FIRMWARE_VERSION "1.6"
+#define SOFTRF_USB_FW_VERSION   0x0106
 
 #define ENTRY_EXPIRATION_TIME   10 /* seconds */
 #define LED_EXPIRATION_TIME     5  /* seconds */
