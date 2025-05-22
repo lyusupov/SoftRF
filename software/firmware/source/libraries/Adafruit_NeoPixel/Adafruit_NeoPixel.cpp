d8a1593a3a21ce710f337b57fd4fe098de1af503
Linar Yusupov <linar.r.yusupov@gmail.com>
2025-05-22 04:46:50 +0300

    [AVR] an update of Adafruit_NeoPixel library

--- software/firmware/source/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.cpp
+++ software/firmware/source/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.cpp
@@ -782,27 +782,27 @@ void Adafruit_NeoPixel::show(void) {
     bit  = 8;
 
     asm volatile(
-     "head20:"                  "\n\t" // Clk  Pseudocode    (T =  0)
+     "head20%=:"                "\n\t" // Clk  Pseudocode    (T =  0)
       "st   %a[port], %[hi]"    "\n\t" // 2    PORT = hi     (T =  2)
       "sbrc %[byte] , 7"        "\n\t" // 1-2  if(b & 128)
        "mov  %[next], %[hi]"    "\n\t" // 0-1   next = hi    (T =  4)
       "st   %a[port], %[next]"  "\n\t" // 2    PORT = next   (T =  6)
       "mov  %[next] , %[lo]"    "\n\t" // 1    next = lo     (T =  7)
       "dec  %[bit]"             "\n\t" // 1    bit--         (T =  8)
-      "breq nextbyte20"         "\n\t" // 1-2  if(bit == 0)
+      "breq nextbyte20%="       "\n\t" // 1-2  if(bit == 0)
       "rol  %[byte]"            "\n\t" // 1    b <<= 1       (T = 10)
       "st   %a[port], %[lo]"    "\n\t" // 2    PORT = lo     (T = 12)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 14)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 16)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 18)
-      "rjmp head20"             "\n\t" // 2    -> head20 (next bit out)
-     "nextbyte20:"              "\n\t" //                    (T = 10)
+      "rjmp head20%="           "\n\t" // 2    -> head20 (next bit out)
+     "nextbyte20%=:"            "\n\t" //                    (T = 10)
       "st   %a[port], %[lo]"    "\n\t" // 2    PORT = lo     (T = 12)
       "nop"                     "\n\t" // 1    nop           (T = 13)
       "ldi  %[bit]  , 8"        "\n\t" // 1    bit = 8       (T = 14)
       "ld   %[byte] , %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 16)
       "sbiw %[count], 1"        "\n\t" // 2    i--           (T = 18)
-      "brne head20"             "\n"   // 2    if(i != 0) -> (next byte)
+      "brne head20%=            "\n"   // 2    if(i != 0) -> (next byte)
       : [port]  "+e" (port),
         [byte]  "+r" (b),
         [bit]   "+r" (bit),
@@ -1153,28 +1153,28 @@ void Adafruit_NeoPixel::show(void) {
     bit  = 8;
 
     asm volatile(
-     "head20:"                   "\n\t" // Clk  Pseudocode    (T =  0)
+     "head20%=:"                 "\n\t" // Clk  Pseudocode    (T =  0)
       "st   %a[port],  %[hi]"    "\n\t" // 2    PORT = hi     (T =  2)
       "sbrc %[byte],  7"         "\n\t" // 1-2  if(b & 128)
        "mov  %[next], %[hi]"     "\n\t" // 0-1   next = hi    (T =  4)
       "dec  %[bit]"              "\n\t" // 1    bit--         (T =  5)
       "st   %a[port],  %[next]"  "\n\t" // 2    PORT = next   (T =  7)
       "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (T =  8)
-      "breq nextbyte20"          "\n\t" // 1-2  if(bit == 0) (from dec above)
+      "breq nextbyte20%="        "\n\t" // 1-2  if(bit == 0) (from dec above)
       "rol  %[byte]"             "\n\t" // 1    b <<= 1       (T = 10)
       "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 12)
       "nop"                      "\n\t" // 1    nop           (T = 13)
       "st   %a[port],  %[lo]"    "\n\t" // 2    PORT = lo     (T = 15)
       "nop"                      "\n\t" // 1    nop           (T = 16)
       "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 18)
-      "rjmp head20"              "\n\t" // 2    -> head20 (next bit out)
-     "nextbyte20:"               "\n\t" //                    (T = 10)
+      "rjmp head20%="            "\n\t" // 2    -> head20 (next bit out)
+     "nextbyte20%=:"             "\n\t" //                    (T = 10)
       "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 11)
       "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 13)
       "st   %a[port], %[lo]"     "\n\t" // 2    PORT = lo     (T = 15)
       "nop"                      "\n\t" // 1    nop           (T = 16)
       "sbiw %[count], 1"         "\n\t" // 2    i--           (T = 18)
-       "brne head20"             "\n"   // 2    if(i != 0) -> (next byte)
+       "brne head20%="           "\n"   // 2    if(i != 0) -> (next byte)
       : [port]  "+e" (port),
         [byte]  "+r" (b),
         [bit]   "+r" (bit),
@@ -1200,7 +1200,7 @@ void Adafruit_NeoPixel::show(void) {
     bit  = 8;
 
     asm volatile(
-     "head40:"                  "\n\t" // Clk  Pseudocode    (T =  0)
+     "head40%=:"                "\n\t" // Clk  Pseudocode    (T =  0)
       "st   %a[port], %[hi]"    "\n\t" // 2    PORT = hi     (T =  2)
       "sbrc %[byte] , 7"        "\n\t" // 1-2  if(b & 128)
        "mov  %[next] , %[hi]"   "\n\t" // 0-1   next = hi    (T =  4)
@@ -1216,7 +1216,7 @@ void Adafruit_NeoPixel::show(void) {
       "nop"                     "\n\t" // 1    nop           (T = 23)
       "mov  %[next] , %[lo]"    "\n\t" // 1    next = lo     (T = 24)
       "dec  %[bit]"             "\n\t" // 1    bit--         (T = 25)
-      "breq nextbyte40"         "\n\t" // 1-2  if(bit == 0)
+      "breq nextbyte40%="       "\n\t" // 1-2  if(bit == 0)
       "rol  %[byte]"            "\n\t" // 1    b <<= 1       (T = 27)
       "nop"                     "\n\t" // 1    nop           (T = 28)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 30)
@@ -1224,15 +1224,15 @@ void Adafruit_NeoPixel::show(void) {
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 34)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 36)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 38)
-      "rjmp head40"             "\n\t" // 2    -> head40 (next bit out)
-     "nextbyte40:"              "\n\t" //                    (T = 27)
+      "rjmp head40%="           "\n\t" // 2    -> head40 (next bit out)
+     "nextbyte40%=:"            "\n\t" //                    (T = 27)
       "ldi  %[bit]  , 8"        "\n\t" // 1    bit = 8       (T = 28)
       "ld   %[byte] , %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 30)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 32)
       "st   %a[port], %[lo]"    "\n\t" // 2    PORT = lo     (T = 34)
       "rjmp .+0"                "\n\t" // 2    nop nop       (T = 36)
       "sbiw %[count], 1"        "\n\t" // 2    i--           (T = 38)
-      "brne head40"             "\n"   // 1-2  if(i != 0) -> (next byte)
+      "brne head40%="           "\n"   // 1-2  if(i != 0) -> (next byte)
       : [port]  "+e" (port),
         [byte]  "+r" (b),
         [bit]   "+r" (bit),
