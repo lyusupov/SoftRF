0b03eb5a06accede009726507f43d240288bedcc
Linar Yusupov <linar.r.yusupov@gmail.com>
2025-11-28 18:13:41 +0300

    [ESP32] add an I/O map for LilyGO T-Lora Dual [skip ci]

--- software/firmware/source/SoftRF/src/platform/ESP32.h
+++ software/firmware/source/SoftRF/src/platform/ESP32.h
@@ -310,7 +310,11 @@ static inline color_t uni_Color(uint8_t r, uint8_t g, uint8_t b) {
 #include "iomap/Banana_PicoW.h"
 #include "iomap/Elecrow_ThinkNode_M2.h"
 #include "iomap/Elecrow_ThinkNode_M5.h"
+#if defined(CONFIG_IDF_TARGET_ESP32)
+#include "iomap/LilyGO_ELRS.h"
+#else
 #include "iomap/Generic_ELRS_C3.h"
+#endif /* CONFIG_IDF_TARGET_ESP32 */
 #include "iomap/Ebyte_EoRa_HUB_900TB.h"
 #include "iomap/WT99P4C5.h"
 #include "iomap/LilyGO_TDisplay_P4.h"
@@ -353,6 +357,7 @@ enum esp32_board_id {
   ESP32_ELECROW_TN_M2,
   ESP32_ELECROW_TN_M5,
   ESP32_RADIOMASTER_XR1,
+  ESP32_LILYGO_T_ELRS,
   ESP32_EBYTE_HUB_900TB,
 };
 
---
+++ software/firmware/source/SoftRF/src/platform/iomap/LilyGO_ELRS.h
@@ -0,0 +1,44 @@
+/*
+ * LilyGO T-Lora Dual LR, a.k.a. LilyGO T-ELRS-LR1121
+ * ESP32-PICO-D4
+ */
+
+#define SOC_GPIO_PIN_ELRS_CONS_RX         12 /* NC */
+#define SOC_GPIO_PIN_ELRS_CONS_TX         23 /* NC */
+
+#define SOC_GPIO_PIN_ELRS_MAV_RX          3  /* U0RXD */
+#define SOC_GPIO_PIN_ELRS_MAV_TX          1  /* U0TXD */
+
+// SPI
+#define SOC_GPIO_PIN_ELRS_MOSI            32
+#define SOC_GPIO_PIN_ELRS_MISO            33
+#define SOC_GPIO_PIN_ELRS_SCK             25
+
+// LR1121
+#if 1
+// Radio #1
+#define SOC_GPIO_PIN_ELRS_SS              27
+
+#define SOC_GPIO_PIN_ELRS_BUSY            36
+#define SOC_GPIO_PIN_ELRS_RST             26
+#define SOC_GPIO_PIN_ELRS_DIO9            37
+
+#define SOC_GPIO_PIN_ELRS_ANT_SW_TX       14
+#define SOC_GPIO_PIN_ELRS_ANT_SW_RX       10
+#else
+// Radio #2
+#define SOC_GPIO_PIN_ELRS_SS              13
+
+#define SOC_GPIO_PIN_ELRS_BUSY            39
+#define SOC_GPIO_PIN_ELRS_RST             21
+#define SOC_GPIO_PIN_ELRS_DIO9            34
+
+#define SOC_GPIO_PIN_ELRS_ANT_SW_TX       15
+#define SOC_GPIO_PIN_ELRS_ANT_SW_RX       9
+#endif
+
+// misc.
+#define SOC_GPIO_PIN_ELRS_LED             SOC_UNUSED_PIN
+#define SOC_GPIO_PIN_ELRS_PIXEL           5
+#define SOC_GPIO_PIN_ELRS_BUTTON          0
+#define SOC_GPIO_PIN_ELRS_BATTERY         35 /* NC */
