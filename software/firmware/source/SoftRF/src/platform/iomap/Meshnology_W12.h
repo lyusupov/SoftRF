3a67ba3ac6b532605573297e27827b1bb15ba03c
Linar Yusupov <linar.r.yusupov@gmail.com>
2026-07-09 16:51:57 +0300

    [ESP32-S3] add an I/O map for Meshnology W12 [skip ci]

---
+++ software/firmware/source/SoftRF/src/platform/iomap/Meshnology_W12.h
@@ -0,0 +1,41 @@
+
+/* Peripherals */
+#define SOC_GPIO_PIN_W12_CONS_RX     44
+#define SOC_GPIO_PIN_W12_CONS_TX     43
+
+// L76K
+#define SOC_GPIO_PIN_W12_GNSS_RX     38
+#define SOC_GPIO_PIN_W12_GNSS_TX     39
+#define SOC_GPIO_PIN_W12_GNSS_PPS    41
+#define SOC_GPIO_PIN_W12_GNSS_RST    42
+#define SOC_GPIO_PIN_W12_GNSS_WKE    40
+#define SOC_GPIO_PIN_W12_GNSS_EN     34
+
+// LR2021
+#define SOC_GPIO_PIN_W12_MOSI        10
+#define SOC_GPIO_PIN_W12_MISO        11
+#define SOC_GPIO_PIN_W12_SCK         9
+#define SOC_GPIO_PIN_W12_SS          8
+#define SOC_GPIO_PIN_W12_RST         12
+#define SOC_GPIO_PIN_W12_BUSY        13
+#define SOC_GPIO_PIN_W12_DIO8        14
+
+// GC1109 FEM
+#define SOC_GPIO_PIN_W12_PA_EN_M     6  /* 868/915 MHz */
+// RFX2402E FEM
+#define SOC_GPIO_PIN_W12_PA_EN_G     7  /* 2.4 GHz */
+
+// I2C bus
+#define SOC_GPIO_PIN_W12_SDA         17
+#define SOC_GPIO_PIN_W12_SCL         18
+
+// 0.96-inch OLED, SSD1315, 128?64, yellow and blue
+#define SOC_GPIO_PIN_W12_OLED_RST    21
+
+// LED
+#define SOC_GPIO_PIN_W12_LED         46 /* NeoPixel */
+
+// Misc.
+#define SOC_GPIO_PIN_W12_ADC         1  /* battery voltage */
+#define SOC_GPIO_PIN_W12_ADC_EN      37
+#define SOC_GPIO_PIN_W12_VEXT_EN     36
