75938f3fdd9eb24c1a889728128fe6e749a8bedd
Linar Yusupov <linar.r.yusupov@gmail.com>
2026-07-09 17:57:58 +0300

    [STM32] add few illustrations for the Retro Mk.II wiki [skip ci]

--- software/firmware/source/SoftRF/src/platform/iomap/Meshnology_W12.h
+++ software/firmware/source/SoftRF/src/platform/iomap/Meshnology_W12.h
@@ -9,7 +9,7 @@
 #define SOC_GPIO_PIN_W12_GNSS_PPS    41
 #define SOC_GPIO_PIN_W12_GNSS_RST    42
 #define SOC_GPIO_PIN_W12_GNSS_WKE    40
-#define SOC_GPIO_PIN_W12_GNSS_EN     34
+#define SOC_GPIO_PIN_W12_GNSS_EN     48
 
 // LR2021
 #define SOC_GPIO_PIN_W12_MOSI        10
@@ -18,24 +18,26 @@
 #define SOC_GPIO_PIN_W12_SS          8
 #define SOC_GPIO_PIN_W12_RST         12
 #define SOC_GPIO_PIN_W12_BUSY        13
-#define SOC_GPIO_PIN_W12_DIO8        14
+#define SOC_GPIO_PIN_W12_DIO5        14
+#define SOC_GPIO_PIN_W12_DIO6        7
 
 // GC1109 FEM
-#define SOC_GPIO_PIN_W12_PA_EN_M     6  /* 868/915 MHz */
+#define SOC_GPIO_PIN_W12_PA_EN_M     4  /* 868/915 MHz */
 // RFX2402E FEM
-#define SOC_GPIO_PIN_W12_PA_EN_G     7  /* 2.4 GHz */
+#define SOC_GPIO_PIN_W12_PA_EN_G     3  /* 2.4 GHz */
 
 // I2C bus
 #define SOC_GPIO_PIN_W12_SDA         17
 #define SOC_GPIO_PIN_W12_SCL         18
 
 // 0.96-inch OLED, SSD1315, 128?64, yellow and blue
-#define SOC_GPIO_PIN_W12_OLED_RST    21
+#define SOC_GPIO_PIN_W12_OLED_RST    47
 
 // LED
 #define SOC_GPIO_PIN_W12_LED         46 /* NeoPixel */
 
 // Misc.
 #define SOC_GPIO_PIN_W12_ADC         1  /* battery voltage */
-#define SOC_GPIO_PIN_W12_ADC_EN      37
-#define SOC_GPIO_PIN_W12_VEXT_EN     36
+#define SOC_GPIO_PIN_W12_ADC2        2  /* solar/VUSB */
+//#define SOC_GPIO_PIN_W12_ADC_EN      37
+#define SOC_GPIO_PIN_W12_VEXT_EN     45
