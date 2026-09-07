a4c21fc45b251ab2e3b3a543ab69771e63328ef6
Linar Yusupov <linar.r.yusupov@gmail.com>
2026-09-07 08:56:53 +0300

    [ESP32-P4] reduce memory space allocated by camera module when able [skip ci]

--- software/firmware/source/SoftRF/src/platform/ESP32.cpp
+++ software/firmware/source/SoftRF/src/platform/ESP32.cpp
@@ -2875,6 +2875,7 @@ static void ESP32_setup()
       cfg.scl = SOC_GPIO_PIN_P4_SCL;
       cfg.pixel_format = ESP32P4_PIXFORMAT_RGB565;
       cfg.sensor = ESP32P4_SENSOR_OV5647;
+      cfg.frame_size = ESP32P4_FRAMESIZE_VGA;
 
       if (cam.begin(cfg)) {
         hw_info.camera = CAMERA_OV5647;
